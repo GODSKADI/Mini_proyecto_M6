894b2ddf327d6c878935912be4965511bd377724
GODSKADI <aidrean.gf@gmail.com>
2018-10-01 20:14:51 +0200

    enunciados

---
+++ script/script.js
@@ -0,0 +1,26 @@
+
+
+//Funcion que comprueba las respuestas para saver si son correctas o incorrectas
+function comprobarRespuestas(e){
+  /*var elemntoHTML;
+  if(e.srcElement){
+    elemntoHTML = e.srcElement;
+  }else if(e.target){
+    elemntoHTML = e.target;
+  }*/
+}
+
+//Funcion que permite limpiar las preguntas seleccionadas
+function limpiar(){
+
+}
+
+
+
+//Funcion que lleva la cuenta de intentos realizados
+//Variable intentos
+
+intentos = 0;
+function intentos(){
+
+}
